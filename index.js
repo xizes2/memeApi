ffeba1a94b0179858162eabe0777ed1838b6eedd
Fernando Paulino <xizes2@hotmail.com>
2022-08-04 00:19:26 +0200

    change port and json file name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("pokemon.json");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 server.use(cors());
 server.use(router);
