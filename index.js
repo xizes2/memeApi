335f95e6f37f44464cdeb68b97f782f2f16bbd7d
Fernando Paulino <xizes2@hotmail.com>
2022-08-04 00:40:26 +0200

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("meme.json");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(router);
