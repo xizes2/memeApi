a84ef31508c802b4c985e5e82a2acc5ec678d85f
Fernando Paulino <xizes2@hotmail.com>
2022-08-04 00:28:00 +0200

    change port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("pokemon.json");
-const port = process.env.PORT || 3001;
+const router = jsonServer.router("meme.json");
+const port = process.env.PORT || 4000;
 
 server.use(cors());
 server.use(router);
